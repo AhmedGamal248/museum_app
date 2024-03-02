100d32574574407213828f13c5bbdfcf90485aab
AhmedGamal248 <ag2849315@gmail.com>
2024-03-03 00:14:02 +0200

    tst

--- databases/DB_connection.js
+++ databases/DB_connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //
 
 export const DBconnection = () => {
-    mongoose.connect(process.env.BASE_URL).then(()=>{
+    mongoose.connect("mongodb+srv://ahmed:ahmedgamal@cluster0.41ojruz.mongodb.net/graduation_P_DB").then(()=>{
     // mongoose.connect("mongodb://127.0.0.1:27017/graduation_P_DB").then(()=>{
         console.log('database connected successfuly...')
     }).catch((err)=>{
